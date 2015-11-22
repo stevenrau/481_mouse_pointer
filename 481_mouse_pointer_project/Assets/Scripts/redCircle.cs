8d889b24b51587887793726cb43d876cd518188b
lendvoyj <johntheleatherman@gmail.com>
2015-11-22 14:53:58 -0600

    Added red circle for the user's pointer
    -> so it is not confused with the real pointer
    -> added script redCirle.cs to the main camera in all scenes
    -> also disabling the real mouse when the game begins

---
+++ 481_mouse_pointer_project/Assets/Scripts/redCircle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class redCircle : MonoBehaviour {
+
+	public Texture circleImage; // the image to use as a new mouse pointer
+	float diameter = 40.0f;
+	float radius;
+
+	float xCorrection = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		radius = diameter / 2.0f;
+		Cursor.visible = false;
+	}
+
+
+	public void OnGUI()
+	{
+		Vector3 mousePos = Input.mousePosition;
+
+		Rect pos = new Rect(mousePos.x - radius + xCorrection, (Screen.height - mousePos.y) - radius, diameter, diameter);
+
+		GUI.Label(pos, circleImage);
+	}
+
+	// Update is called once per frame
+	//void Update () {
+	//}
+}
